8f30b34d26b3e1a88627bc15af55c745b92847c6
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-06 22:40:40 +0530

    removed dependency on PaymentGateway. is now only responsible for knowing how to calculate overdue fees.

--- src/main/java/library/PenaltyAccountant.java
+++ src/main/java/library/PenaltyAccountant.java
@@ -7,23 +7,18 @@ import java.util.Map;
 
 public class PenaltyAccountant implements Accountant{
     DataManager<String, User> dataManager;
-    PaymentGateway paymentGateway;
 
 
     public PenaltyAccountant(DataManager<String, User> dataManager, PaymentGateway paymentGateway) {
         this.dataManager = dataManager;
-        this.paymentGateway = paymentGateway;
-        this.paymentGateway.registerAccountant(this);
     }
 
-    public void chargePenalty(User user, int overdueDays){
+    public Double chargePenalty(User user, int overdueDays){
         user = getUpdatedUserData(user);
 
         Map<String, Double> relevantDetails = getRelevantPenaltyDetails(user);
 
-        double totalPenaltyDue = calculatePenalty(relevantDetails, overdueDays);
-
-        forwardToPaymentGateway(user, totalPenaltyDue);
+        return calculatePenalty(relevantDetails, overdueDays);
     }
 
     private User getUpdatedUserData(User user){
@@ -34,14 +29,12 @@ public class PenaltyAccountant implements Accountant{
 
     // find better function name
     private Map<String,Double> getRelevantPenaltyDetails(User user){
-        MembershipLevel membershipLevel = user.getMembershipLevel();
         Double previousPenalties = user.getPenaltyDue();
-        Double overduePerDay = membershipLevel.getOverdueFeesPerDay();
+        Double overduePerDay = user.getOverdueFeesPerDay();
         Map<String,Double> details = Map.of(
                         "previousPenalties", previousPenalties,
                         "overduePerDay",overduePerDay
         );
-
         return details;
     }
 
@@ -53,25 +46,4 @@ public class PenaltyAccountant implements Accountant{
         return previousPenalties + (overdueDays)*overduePerDay;
 
     }
-
-    private void forwardToPaymentGateway(User user, double paymentDue){
-        paymentGateway.initializePayment(user, paymentDue);
-    }
-
-    //To-Do find better name. has side effects
-    public void receivePaymentStatus(boolean status, User user) {
-        if(status==true) {
-            updateUserPenaltyData(user);
-            sendUpdatedDataToManager(user);
-        }
-    }
-
-    private void updateUserPenaltyData(User user){
-        user.setPenaltyDue(0);
-    }
-
-    private void sendUpdatedDataToManager(User user){
-        dataManager.updateData(user);
-    }
-
 }
