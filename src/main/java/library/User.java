ff37cb3bbb34fb63fb9b7e58113486c772a10fb0
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-08 15:06:24 +0530

    has new method getDiscounts() which calls the MembershipLevel method of the same name.

---
+++ src/main/java/library/User.java
@@ -0,0 +1,51 @@
+package library;
+
+import library.membershiplevels.MembershipLevel;
+import libraryitems.LibraryItem;
+
+import java.util.ArrayList;
+
+public class User {
+
+    final int ID;
+
+    MembershipLevel membershipLevel;
+
+    protected ArrayList<LibraryItem> borrowedItems = new ArrayList<>();
+
+    double PenaltyDue;
+
+    public User(int ID, MembershipLevel memberShipLevel, double penaltyDue) {
+        this.ID = ID;
+        this.membershipLevel = memberShipLevel;
+        PenaltyDue = penaltyDue;
+    }
+
+    public Double getOverdueFeesPerDay(){
+        return membershipLevel.getOverdueFeesPerDay();
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getPenaltyDue() {
+        return PenaltyDue;
+    }
+
+    public MembershipLevel getMembershipLevel() {
+        return membershipLevel;
+    }
+
+    public void setMembershipLevel(MembershipLevel membershipLevel) {
+        this.membershipLevel = membershipLevel;
+    }
+
+    public void setPenaltyDue(double penaltyDue) {
+        PenaltyDue = penaltyDue;
+    }
+    public double getDiscounts(){
+        return MembershipLevel.getDiscounts();
+        }
+}
