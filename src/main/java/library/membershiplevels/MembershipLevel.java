73c3fcce4b672625393b85e8b4a1b9befc86e347
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-11 15:38:33 +0530

    Converted from interface to class. this will return a MembershipLevel object depending on a policy passed to it. the policy can be gold, basic, etc.

--- src/main/java/library/membershiplevels/MembershipLevel.java
+++ src/main/java/library/membershiplevels/MembershipLevel.java
@@ -1,50 +1,88 @@
 package library.membershiplevels;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Calendar;
+import java.util.HashMap;
 
-public interface MembershipLevel {
+@Entity
+public class MembershipLevel {
 
+    // subclasses must initialize these values
+    String policy;
+    Double membershipFees;
+    Double overdueFeesPerDay;
+    int borrowLimit;
+    Double discountPercentage; // value must be between 0.0 to 100.0
+    int membershipPeriodInMonths;
 
-    static Double getOverdueFeesPerDay(){
-        return null;
+    @Temporal(TemporalType.TIMESTAMP)
+    Calendar expiringOn;
 
+    MembershipLevel(MembershipPolicy policy){
+        this.policy = policy.toString();
+        this.membershipFees = policy.getMembershipFees();
+        this.overdueFeesPerDay = policy.getOverdueFeesPerDay();
+        this.borrowLimit = policy.getBorrowLimit();
+        this.discountPercentage = policy.getDiscountPercentage();
+        this.membershipPeriodInMonths = policy.getMembershipPeriodInMonths();
+        this.expiringOn = calculateMembershipExpiry();
     }
-    default Calendar calculateMembershipExpiry(int membershipPeriodInMonths ){
+
+    protected Calendar calculateMembershipExpiry() {
         Calendar expiryOn = Calendar.getInstance();
-        expiryOn.add(Calendar.MONTH, membershipPeriodInMonths);
+        expiryOn.add(Calendar.MONTH, this.membershipPeriodInMonths);
         return expiryOn;
     }
 
-    static Double getMembershipFees(){
-        return 0.0;
+    public Double getMembershipFees() {
+        return membershipFees;
     }
 
-    static void setMembershipFees(Double membershipFees){};
-
-
-    static void setOverdueFeesPerDay(Double overdueFeesPerDay){}
-
-    static int getBorrowLimit(){
-        return 0;
+    public void setMembershipFees(Double membershipFees) {
+        this.membershipFees = membershipFees;
     }
 
-    static void setBorrowLimit(int borrowLimit){}
-
-    static Double getDiscounts(){
-        return null;
+    public Double getOverdueFeesPerDay() {
+        return overdueFeesPerDay;
     }
 
-    static void setDiscounts(Double discounts){}
-
-    static int getMembershipPeriodInMonths(){
-        return 0;
+    public void setOverdueFeesPerDay(Double overdueFeesPerDay) {
+        this.overdueFeesPerDay = overdueFeesPerDay;
     }
 
-    static void setMembershipPeriodInMonths(int membershipPeriodInMonths){}
+    public int getBorrowLimit() {
+        return borrowLimit;
+    }
 
-    Calendar getExpiringOn();
+    public void setBorrowLimit(int borrowLimit) {
+        this.borrowLimit = borrowLimit;
+    }
+
+    public Double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(Double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public int getMembershipPeriodInMonths() {
+        return membershipPeriodInMonths;
+    }
+
+    public void setMembershipPeriodInMonths(int membershipPeriodInMonths) {
+        this.membershipPeriodInMonths = membershipPeriodInMonths;
+    }
+
+    public Calendar getExpiringOn() {
+        return expiringOn;
+    }
+
+    public void setExpiringOn(Calendar expiringOn) {
+        this.expiringOn = expiringOn;
+    }
+
 }
-
-
-
