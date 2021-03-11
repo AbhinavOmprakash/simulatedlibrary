6731131ded458eed491c114cc85c28d159286e81
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-11 15:39:33 +0530

    policy objects that implement the MembershipPolicy interface these hold data about the current policy and are used to create MembershipLevel objects

---
+++ src/main/java/library/membershiplevels/basicPolicy.java
@@ -0,0 +1,68 @@
+package library.membershiplevels;
+
+public class basicPolicy implements MembershipPolicy {
+    Double membershipFees;
+    Double overdueFeesPerDay;
+    int borrowLimit;
+    Double discountPercentage; // value must be between 0.0 to 100.0
+    int membershipPeriodInMonths = 12;
+
+    @Override
+    public String toString(){
+        return "Basic membership";
+    }
+
+    @Override
+    public Double getMembershipFees() {
+        return membershipFees;
+    }
+
+    @Override
+    public void setMembershipFees(Double membershipFees) {
+        this.membershipFees = membershipFees;
+    }
+
+    @Override
+    public Double getOverdueFeesPerDay() {
+        return overdueFeesPerDay;
+    }
+
+    @Override
+    public void setOverdueFeesPerDay(Double overdueFeesPerDay) {
+        this.overdueFeesPerDay = overdueFeesPerDay;
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return borrowLimit;
+    }
+
+    @Override
+    public void setBorrowLimit(int borrowLimit) {
+        this.borrowLimit = borrowLimit;
+    }
+
+    @Override
+    public Double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    @Override
+    public void setDiscountPercentage(Double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public int getMembershipPeriodInMonths() {
+        return membershipPeriodInMonths;
+    }
+
+    @Override
+    public void setMembershipPeriodInMonths(int membershipPeriodInMonths) {
+        this.membershipPeriodInMonths = membershipPeriodInMonths;
+    }
+
+
+
+
+}
