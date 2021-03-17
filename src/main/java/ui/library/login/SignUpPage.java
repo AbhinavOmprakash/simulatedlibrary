f568cf3bdc80784dfd97bbd00f052984f66522a4
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-17 21:22:58 +0530

    created a page to sign up users

---
+++ src/main/java/ui/library/login/SignUpPage.java
@@ -0,0 +1,19 @@
+package ui.library.login;
+
+import javax.swing.*;
+
+public class SignUpPage {
+
+    private JPanel panel;
+    private JPanel SignUpPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton signUpButton;
+
+    public JPanel getPanel() {
+        return panel;
+    }
+}
