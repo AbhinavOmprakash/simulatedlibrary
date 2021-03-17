d4b755e83fd813d4a5c9a8ba0a0ab5df3bd39d6e
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-17 17:16:09 +0530

    renamed file

---
+++ src/main/java/backend/libraryitems/Archive.java
@@ -0,0 +1,21 @@
+package backend.libraryitems;
+
+import backend.libraryitems.contributors.Contributor;
+
+import javax.persistence.Entity;
+import java.util.ArrayList;
+
+@Entity
+public class Archive extends LibraryItem{
+    public Archive(){
+        // for hibernate
+    }
+
+    public Archive(String title,
+                   String subject,
+                   int UPC,
+                   ArrayList<Contributor> contributors) {
+        super(title, subject, UPC, contributors, false, 0, "Archive");
+
+    }
+}
