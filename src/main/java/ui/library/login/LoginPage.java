3890eb691dfdfb973cdaf5ff0a8c485575436c7a
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-17 20:58:42 +0530

    created special package for the login related GUI

---
+++ src/main/java/ui/library/login/LoginPage.java
@@ -0,0 +1,20 @@
+package ui.library.login;
+
+import javax.swing.*;
+
+public class LoginPage {
+
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JPanel LoginPanel;
+    private JButton loginButton;
+
+    public LoginPage(){
+
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+}
