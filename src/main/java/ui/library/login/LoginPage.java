f8aba91095a7f176df1178ffcc1acc34871baf22
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-18 11:14:12 +0530

    now implements the actionListener and displayPage interfaces

--- src/main/java/ui/library/login/LoginPage.java
+++ src/main/java/ui/library/login/LoginPage.java
@@ -1,20 +1,47 @@
 package ui.library.login;
 
-import javax.swing.*;
+import backend.controllers.UiObserverable;
+import ui.library.MainPage;
+import ui.library.UiObserver;
+import ui.library.displayPage;
 
-public class LoginPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginPage implements ActionListener, displayPage {
 
     private JPanel panel1;
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JPanel LoginPanel;
     private JButton loginButton;
+    private JButton forgottenPassword;
 
-    public LoginPage(){
+    private ActionListener loginController;
 
+    public LoginPage( ActionListener loginController){
+        forgottenPassword.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+        forgottenPassword.setContentAreaFilled(false);
+
+        this.loginController = loginController;
+        loginButton.addActionListener(loginController);
     }
 
     public JPanel getPanel1() {
         return panel1;
+    }
+
+    @Override
+    public JPanel getPanel() {
+        return getPanel1();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton){
+            loginController.actionPerformed(e);
+        }
+
     }
 }
