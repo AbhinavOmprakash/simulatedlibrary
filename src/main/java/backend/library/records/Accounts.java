ed10469133416948c820de02bec663cf58055208
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-22 12:18:52 +0530

    class to deal with financial matters pertaining the user

---
+++ src/main/java/backend/library/records/Accounts.java
@@ -0,0 +1,65 @@
+package backend.library.records;
+
+import backend.library.User;
+import backend.libraryitems.LibraryItem;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+public class Accounts{
+    @Id
+    private int userID;
+    private double penaltyDue = 0.0;
+    private double totalMoneyPaid = 0.0;
+
+    Accounts(){
+        // for hibernate
+    }
+
+    public Accounts(User user){
+        this.userID = user.getID();
+    }
+
+    public void addPenalty(double penalty){
+        this.penaltyDue = penalty;
+    }
+
+    public void penaltyPaid(double penaltyPaid){
+        this.penaltyDue -= penaltyPaid;
+        this.totalMoneyPaid += penaltyPaid;
+    }
+
+    public void addMembershipFees(double membershipFees){
+        this.totalMoneyPaid += membershipFees;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public double getPenaltyDue() {
+        return penaltyDue;
+    }
+
+    public void setPenaltyDue(double penaltyDue) {
+        this.penaltyDue = penaltyDue;
+    }
+
+    public double getTotalMoneyPaid() {
+        return totalMoneyPaid;
+    }
+
+    public void setTotalMoneyPaid(double totalMoneyPaid) {
+        this.totalMoneyPaid = totalMoneyPaid;
+    }
+
+}
