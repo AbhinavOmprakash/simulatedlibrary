f3a7c68eab669f710c2c079ffc7d32b12a1ce32b
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:32:47 +0530

    couldn't get the jdialog to display so made this return a panel

--- src/main/java/ui/library/login/ForgottenPassword.java
+++ src/main/java/ui/library/login/ForgottenPassword.java
@@ -1,29 +1,34 @@
 package ui.library.login;
 
+import ui.library.displayPage;
+
 import javax.swing.*;
 import java.awt.event.*;
 
-public class ForgottenPassword extends JDialog {
+public class ForgottenPassword extends JDialog implements displayPage {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
     private JTextArea textArea1;
     private JPanel textPanel;
 
-    public ForgottenPassword() {
+    private EntryPage parent;
+
+    public ForgottenPassword(EntryPage parent) {
+        this.parent = parent;
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
         String displayText = "you forgot your password? \n" +
                 "how could you do this to me? \n" +
-                "I trusted you, you know I am doing a lot fo things right now.\n" +
+                "I trusted you, you know I am doing a lot of things right now.\n" +
                 "and this is how you treat me? \n" +
                 "I'm disappointed in you.\n"+
                 "\nI AM JOKING \n" +
                 "this feature is not withing the intended scope of this project so, I'm sorry\n"+
                 "\nSincerely, \n"+
-                "Your beloved JVM.";
+                "This program.";
 
 
         textArea1.setText(displayText);
@@ -59,17 +64,17 @@ public class ForgottenPassword extends JDialog {
     private void onOK() {
         // add your code here
         dispose();
+        parent.changeToLoginPage();
     }
 
     private void onCancel() {
         // add your code here if necessary
         dispose();
+        parent.changeToLoginPage();
     }
 
-    public static void main(String[] args) {
-        ForgottenPassword dialog = new ForgottenPassword();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+    @Override
+    public JPanel getPanel() {
+        return contentPane;
     }
 }
