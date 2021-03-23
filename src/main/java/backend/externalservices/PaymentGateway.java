d06002f585e31574cde355c447a9a772f7ca1dfd
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:44:32 +0530

    removed initializePayment method

--- src/main/java/backend/externalservices/PaymentGateway.java
+++ src/main/java/backend/externalservices/PaymentGateway.java
@@ -5,7 +5,6 @@ import backend.library.Accountant;
 import backend.library.User;
 
 public interface PaymentGateway{
-    void initializePayment(User user, double targetAmount);
     void acceptPayment(User user, double targetAmount);
     void registerAccountant(Accountant accountant);
     void removeAccountant(Accountant accountant);
