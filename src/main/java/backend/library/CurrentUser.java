fef1b53cfcf562aeeb791a325c5e66e89aa5c391
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:30:19 +0530

    created singleton class to provide global point of access to the currently signed in user

---
+++ src/main/java/backend/library/CurrentUser.java
@@ -0,0 +1,22 @@
+package backend.library;
+
+public class CurrentUser{
+    private static User currentUser;
+    private static CurrentUser instance = new CurrentUser();
+
+    private CurrentUser(){}
+
+    public static void changeUser(User user){
+        currentUser = user;
+    }
+    public static void removeUser(){
+        currentUser = null;
+    }
+
+    public static User getCurrentUser() throws UnsupportedOperationException{
+        if(currentUser==null){
+            throw new UnsupportedOperationException("the current user has not been set");
+        }
+        return currentUser;
+    }
+}
