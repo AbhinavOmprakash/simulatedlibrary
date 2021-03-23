2eaee3a18eadd8a86395ef1dd9168ddddaa50e21
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:38:42 +0530

    to handle login info passed. and to store it too

---
+++ src/main/java/backend/library/records/LoginData.java
@@ -0,0 +1,61 @@
+package backend.library.records;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class LoginData {
+    public String username;
+    public String hashedPassword;
+
+    @Id
+    public int userID;
+
+    public LoginData(){
+        // for hibernate
+    }
+    public LoginData(String username, char[] password){
+        this.username = username;
+        this.hashedPassword = BCrypt.hashpw(String.valueOf(password), BCrypt.gensalt());
+    }
+
+    public LoginData(String username, char[] password, int userID){
+        this.username = username;
+        this.hashedPassword = BCrypt.hashpw(String.valueOf(password), BCrypt.gensalt());
+        this.userID = userID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginData loginData = (LoginData) o;
+        return username.equals(loginData.username) && hashedPassword.equals(loginData.hashedPassword);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String password) {
+        hashedPassword = BCrypt.hashpw(password,BCrypt.gensalt());
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
