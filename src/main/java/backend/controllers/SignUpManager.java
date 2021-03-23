d691abdfd771498a447d0e3843c53b58966323d2
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:45:45 +0530

    data manager to handle signing up

---
+++ src/main/java/backend/controllers/SignUpManager.java
@@ -0,0 +1,14 @@
+package backend.controllers;
+
+import backend.library.records.SignUpData;
+
+public class SignUpManager{
+    DataManager userDataManager = new UserDataManager();
+
+    public boolean signUp(SignUpData data){
+//        User user = new User(data.firstName, data.lastName
+        return true;
+    }
+
+
+}
