3b2f098a0b04157749859da3204d23905defcc61
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 19:33:24 +0530

    gui to handle signing up

--- src/main/java/ui/library/login/SignUpPage.java
+++ src/main/java/ui/library/login/SignUpPage.java
@@ -1,8 +1,18 @@
 package ui.library.login;
 
-import javax.swing.*;
+import backend.controllers.MembershipPolicyManager;
+import backend.controllers.SignUpManager;
+import backend.library.records.SignUpData;
+import ui.library.displayPage;
 
-public class SignUpPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class SignUpPage implements displayPage, ActionListener {
 
     private JPanel panel;
     private JPanel SignUpPanel;
@@ -12,8 +22,67 @@ public class SignUpPage {
     private JPasswordField passwordField1;
     private JPasswordField passwordField2;
     private JButton signUpButton;
+    private JButton haveAnAccountButton;
+    private JSpinner membershipSpinner;
+    private JLabel membershipPolicy;
+
+    EntryPage parent;
+
+    SignUpManager signUpManager = new SignUpManager();
+    MembershipPolicyManager policyManager = new MembershipPolicyManager();
+
+    public SignUpPage(EntryPage parent) {
+        this.parent  = parent;
+        signUpButton.addActionListener(this);
+        haveAnAccountButton.addActionListener(this);
+//        populateSpinner();
+    }
+
+    private void populateSpinner(){
+        SpinnerListModel policies = new SpinnerListModel(getAllPolicies());
+        membershipSpinner.setValue((Object) policies);
+        System.out.println("populated");
+    }
+
+    private Object[] getAllPolicies(){
+        ArrayList  policies = policyManager.fetchAll();
+        // for dev
+        if (policies.isEmpty()) {
+            return new Object[]{"basic"};
+        }else {
+            return policies.toArray();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==signUpButton){
+            performSignUp();
+        }
+        if (e.getSource()==haveAnAccountButton){
+            parent.changeToLoginPage();
+        }
+
+    }
+
+    private void performSignUp() {
+        if(true){
+            parent.signUpSuccessful();
+        }
+    }
+
+    private boolean trySigningUp() {
+        SignUpData signUpData = fetchSignUpData();
+        return true;
+    }
+
+    private SignUpData fetchSignUpData() {
+        return null;
+    }
 
     public JPanel getPanel() {
         return panel;
     }
+
+
 }
