30911f84fcea65662cf0411a8c2122f1430629ad
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:39:46 +0530

    removed unused methods

--- src/main/java/ui/library/MainJFrame.java
+++ src/main/java/ui/library/MainJFrame.java
@@ -1,25 +1,28 @@
 package ui.library;
 
-import backend.controllers.UiObserverable;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class MainJFrame extends JFrame implements UiObserver {
-    displayPage mainPage = new MainPage(this);
+public class MainJFrame extends JFrame implements PropertyChangeListener, ActionListener {
+    JPanel mainPage =  new MainPage(this);
 
     public MainJFrame(String title) {
         super(title);
-        setContentPane(mainPage.getPanel());
+        setContentPane(mainPage);
 
     }
 
     @Override
-    public void performAction(Object source) {
-        setContentPane(mainPage.getPanel());
+    public void propertyChange(PropertyChangeEvent evt) {
         repaint();
         revalidate();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
