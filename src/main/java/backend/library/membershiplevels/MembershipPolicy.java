f141a845dcb023d8c6c78659204345adb9025c69
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-23 17:42:28 +0530

    made it a pure data object. removed the unnecessary getters and setters

--- src/main/java/backend/library/membershiplevels/MembershipPolicy.java
+++ src/main/java/backend/library/membershiplevels/MembershipPolicy.java
@@ -1,22 +1,40 @@
 package backend.library.membershiplevels;
 
-public interface MembershipPolicy {
+import org.hibernate.annotations.GenericGenerator;
 
-    String toString();
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-    //getters
-    Double getMembershipFees();
-    Double getOverdueFeesPerDay();
-    int getBorrowLimit();
-    Double getDiscountPercentage();
-    int getMembershipPeriodInMonths();
+@Entity
+public class MembershipPolicy {
 
-    //setters
-    void setMembershipFees(Double membershipFees);
-    void setOverdueFeesPerDay(Double overdueFeesPerDay);
-    void setBorrowLimit(int borrowLimit);
-    void setDiscountPercentage(Double discountPercentage);
-    void setMembershipPeriodInMonths(int membershipPeriodInMonths);
+    public String name;
+    public Double membershipFees;
+    public Double overdueFeesPerDay;
+    public int borrowLimit;
+    public Double discountPercentage; // value must be between 0.0 to 100.0
+    public int membershipPeriodInMonths = 12;
 
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public int Id;
+
+    public MembershipPolicy(String name, Double membershipFees,
+                            Double overdueFeesPerDay, int borrowLimit,
+                            Double discountPercentage,
+                            int membershipPeriodInMonths) {
+        this.name = name;
+        this.membershipFees = membershipFees;
+        this.overdueFeesPerDay = overdueFeesPerDay;
+        this.borrowLimit = borrowLimit;
+        this.discountPercentage = discountPercentage;
+        this.membershipPeriodInMonths = membershipPeriodInMonths;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
