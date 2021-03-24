80176707045520173cb131f265e8f2f2b4e306d7
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-24 18:52:54 +0530

    To - DO find a better name for the class

--- src/main/java/backend/controllers/Librarian.java
+++ src/main/java/backend/controllers/Librarian.java
@@ -6,6 +6,7 @@ import backend.dataobjects.libraryitems.LibraryItem;
 
 import java.util.ArrayList;
 
+// To Do find a better name for class
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class Librarian {
     DataManager borrowedItems = BorrowedItemsDataManager.getInstanceOf();
