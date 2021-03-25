7f17919847799ae232157bc333a60383d07233f5
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-25 16:29:41 +0530

    now the factory saves the newly created item

--- src/main/java/models/dataobjects/libraryitems/factories/LibraryItemFactory.java
+++ src/main/java/models/dataobjects/libraryitems/factories/LibraryItemFactory.java
@@ -1,11 +1,17 @@
 package models.dataobjects.libraryitems.factories;
 
+import controllers.DataManager;
+import controllers.library.LibraryItemManager;
 import models.dataobjects.libraryitems.LibraryItem;
 import models.dataobjects.libraryitems.contributors.Contributor;
 
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
 
+@SuppressWarnings({"unchecked, rawtypes"})
 public class LibraryItemFactory {
+    private static DataManager libraryItems = new LibraryItemManager();
+
     public static LibraryItem createNewItem(NewLibraryItemData data){
         ArrayList<Contributor> contributors = ContributorFactory.getContributors(data.contributorsWithType);
         LibraryItem newItem;
@@ -16,6 +22,7 @@ public class LibraryItemFactory {
             case("DVD") -> newItem = DvdFactory.createNew(data, contributors);
             default -> newItem = BookFactory.createNew(data, contributors);
         }
+        libraryItems.addItem(newItem);
         return newItem;
     }
 }
