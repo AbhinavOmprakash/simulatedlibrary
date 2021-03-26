9556852ac0e4f993f645a594aa8da794d1ab40e0
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-26 10:37:14 +0530

    added functionality

--- src/main/java/externalservices/DummyPayment.java
+++ src/main/java/externalservices/DummyPayment.java
@@ -2,31 +2,44 @@ package externalservices;
 
 import controllers.financials.AccountsDataManager;
 import controllers.DataManager;
-import controllers.financials.Accountant;
-import models.dataobjects.library.User;
+import controllers.useraccounts.PaymentObserver;
+import models.dataobjects.library.records.FinancialAccount;
 
+import java.util.ArrayList;
+
+@SuppressWarnings({"rawtypes","unchecked"})
 public class DummyPayment implements PaymentGateway{
-    DataManager accounts = new AccountsDataManager();
-
+    DataManager financialAccounts = new AccountsDataManager();
+    ArrayList<PaymentObserver> observers = new ArrayList<>();
 
 
     @Override
-    public void acceptPayment(User user, double targetAmount) {
+    public void acceptPayment(Long userID, double targetAmount) {
+        notifyPaymentStatus(userID, true);
+        updateAccounts(userID, targetAmount);
+    }
 
+    private void updateAccounts(Long userID, double targetAmount) {
+        FinancialAccount account = new FinancialAccount(userID);
+        account.addMembershipFees(targetAmount);
+//        financialAccounts.updateData(account);
+        financialAccounts.addItem(account);
     }
 
     @Override
-    public void registerAccountant(Accountant accountant) {
-
+    public void registerObservers(PaymentObserver o) {
+        observers.add(o);
     }
 
     @Override
-    public void removeAccountant(Accountant accountant) {
-
+    public void removeObservers(PaymentObserver o) {
+        observers.remove(o);
     }
 
     @Override
-    public void notifyPaymentStatus(Accountant accountant, boolean status) {
-
+    public void notifyPaymentStatus(Long userID, boolean status) {
+        for(PaymentObserver o: observers){
+            o.receivePaymentStatus(userID, status);
+        }
     }
 }
