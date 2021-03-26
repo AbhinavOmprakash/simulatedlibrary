22cadbdb24be728d4e5c8fe8e7057bbf4172e203
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-26 22:12:38 +0530

    created enum that has access to all the views in the app. solves the problem of requesting pages from different apps

---
+++ src/main/java/common/Page.java
@@ -0,0 +1,11 @@
+package common;
+
+public enum Page {
+    // register your app views here.
+    
+    LOGIN, // login
+    SIGN_UP, // sign up 
+    ADMIN_HOME, NEW_LIB_ITEM, NEW_POLICY, // admin
+    MEMBER_HOME, // library
+    MEMBER_ACCOUNT, UPGRADE // member
+}
