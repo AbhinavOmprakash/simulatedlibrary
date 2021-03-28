281124f09497c48cc6c32046765db984fbca1d83
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-28 21:51:52 +0530

    decoupled from Router, and now is only coupled to its displayPage.

--- src/main/java/admin/controllers/AdminHomeController.java
+++ src/main/java/admin/controllers/AdminHomeController.java
@@ -1,27 +1,19 @@
 package admin.controllers;
 
-import common.controllers.GuiController;
-import common.controllers.MainFrameController;
-import common.views.MainJFrame;
 import admin.views.AdminHome;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AdminHomeController extends GuiController implements ActionListener {
-    AdminHome adminHome = new AdminHome(this);
-    public AdminHomeController(MainFrameController parentController, MainJFrame mainFrame) {
-        super(parentController, mainFrame);
-        setCurrentPage(adminHome);
+public class AdminHomeController implements ActionListener {
+    AdminHome adminHome;
+
+    public AdminHomeController(AdminHome page) {
+        adminHome = page;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==adminHome.addNewLibraryItemButton){
-            parentController.switchToNewLibraryItem();
-        } else if(e.getSource()==adminHome.addNewPolicyButton){
-            parentController.switchToNewMembershipPolicy();
-        }
 
     }
 }
