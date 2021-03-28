d260f2939ea101597e73f0422909e8e876a9403e
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-28 21:58:23 +0530

    enum to pass custom events

---
+++ src/main/java/common/customevents/CustomEvent.java
@@ -0,0 +1,10 @@
+package common.customevents;
+
+public enum CustomEvent {
+    // register custom events here
+    SIGNED_UP,
+    LOGGED_IN,
+    PAYMENT_GATEWAY,
+    NEW_PAYMENT,
+    LOG_OUT,
+}
