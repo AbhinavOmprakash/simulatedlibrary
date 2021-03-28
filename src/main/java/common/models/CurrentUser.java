e94f95782a178e6d4f0c8895b889a4eeba0d0582
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-28 21:57:53 +0530

    implements the EventListener interface. it listens for the LOG_OUT event, and saves the user, and "logs out".

--- src/main/java/common/models/CurrentUser.java
+++ src/main/java/common/models/CurrentUser.java
@@ -1,26 +1,19 @@
 package common.models;
 
-import member.controllers.UserDataManager;
+import common.customevents.CustomEvent;
+import common.customevents.EventListener;
+import member.models.UserDataManager;
 
 import java.util.ArrayList;
 
 @SuppressWarnings({"unchecked,rawtypes"})
-public class CurrentUser{
+public class CurrentUser implements EventListener {
     private static User currentUser;
     private static CurrentUser instance = new CurrentUser();
 
     private static ArrayList<DataObserver> observers = new ArrayList<>();
 
     private CurrentUser(){}
-
-    public static void changeUser(User user){
-        saveUserData();
-        currentUser = user;
-    }
-    public static void removeUser(){
-        saveUserData();
-        currentUser = null;
-    }
 
     public static User getCurrentUser() throws UnsupportedOperationException{
         if(currentUser==null){
@@ -29,11 +22,27 @@ public class CurrentUser{
         return currentUser;
     }
 
+    @Override
+    public void receive(CustomEvent event) {
+        if(event.equals(CustomEvent.LOG_OUT)){
+            removeUser();
+        }
+    }
+
+    private static void removeUser(){
+        saveUserData();
+        currentUser = null;
+    }
+
+    public static void changeUser(User user){
+        saveUserData();
+        currentUser = user;
+    }
+
     public static void saveUserData(){
         if(currentUser!=null){
             UserDataManager.getInstanceOf().updateData(currentUser);
         }
-
     }
 
 }
