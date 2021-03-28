cc69e3ccb0616cef3b0226934965941d0d77dd85
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-28 21:41:53 +0530

    enum for routers to route views from one app to another

---
+++ src/main/java/common/Views.java
@@ -0,0 +1,13 @@
+package common;
+
+public enum Views {
+    // register your app views here.
+
+    LOGIN, // login
+    SIGN_UP, // sign up
+    ADMIN_HOME, // admin
+    LIBRARY_HOME, // library
+    MEMBER_HOME, // member
+    PAYMENT, // payment
+    BACK,
+}
