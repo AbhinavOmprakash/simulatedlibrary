cc43fd8fdfafa25e432e10da8c9674369c3c5815
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-28 22:21:27 +0530

    LoginManager verifies the login and then dispatches the LOGGED_IN signal to the main router for changing the views.

--- src/main/java/login/models/LoginManager.java
+++ src/main/java/login/models/LoginManager.java
@@ -1,37 +1,40 @@
 package login.models;
 
+import common.customevents.CustomEvent;
+import common.customevents.EventCotroller;
 import common.models.CurrentUser;
 import common.models.User;
-import member.controllers.UserDataManager;
+import member.models.UserDataManager;
 
 public class LoginManager {
     LoginDataManager loginData = new LoginDataManager();
     UserDataManager userdata = UserDataManager.getInstanceOf();
 
-    public boolean login(LoginData enteredData){
-        //todo change in production
-        if(true){
+    public void login(rawLoginData enteredData){
+        if(verifyLoginData(enteredData)){
             changeUser(enteredData);
-            return true;
-        } else {
-            return false;
+            dispatchSignal();
         }
+        changeUser(enteredData);
+        dispatchSignal();
     }
 
-    private void changeUser(LoginData data) {
+    private void changeUser(rawLoginData data) {
         User user = getUserFromDB(data);
         CurrentUser.changeUser(user);
     }
 
-    //todo modify in production
-    private User getUserFromDB(LoginData data) {
-        return (User) userdata.search("abhi").get(0);
+    private void dispatchSignal() {
+        EventCotroller.getInstanceOf().dispatch(CustomEvent.LOGGED_IN);
     }
 
-    private boolean verifyLoginData(LoginData enteredData) {
+    private User getUserFromDB(rawLoginData data) {
+        return (User) userdata.search(data.getUsername()).get(0);
+    }
+
+    private boolean verifyLoginData(rawLoginData enteredData) {
         LoginData storedData = (LoginData) this.loginData.search(enteredData.getUsername()).get(0);
-        return storedData.equals(enteredData);
+        return storedData.verifyPasswd(enteredData);
     }
-
 
 }
