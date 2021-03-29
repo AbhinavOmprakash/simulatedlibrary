b709251505d7deecbefbc7adc2c3e37ebc1ea1a3
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 22:12:31 +0530

    constructor takes in a DataManager now.

--- src/main/java/library/models/ReturnIncharge.java
+++ src/main/java/library/models/ReturnIncharge.java
@@ -4,11 +4,16 @@ import common.models.DataManager;
 import common.models.Member;
 import library.models.libraryitems.LibraryItem;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
-public class ReturnIncharge {
-    private static DataManager borrowedItems = BorrowedItemsDataManager.getInstanceOf();
+import java.time.LocalDateTime;
 
-    public static void letUserReturn(Member user, LibraryItem item){
+public class ReturnIncharge {
+    DataManager borrowedItemsRecord;
+
+    public ReturnIncharge(DataManager borrowedItemsRecord) {
+        this.borrowedItemsRecord = borrowedItemsRecord;
+    }
+
+    public void returnItem(Member user, LibraryItem item){
         user.returnBorrowedItem(item);
         removeFromBorrowedItems(item);
     }
