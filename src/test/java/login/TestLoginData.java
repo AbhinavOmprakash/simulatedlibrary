ccf97ffa189096edbd45ececf87bedb52340f3aa
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 10:07:18 +0530

    test that login data is purely a data object and has no methods or side effects

---
+++ src/test/java/login/TestLoginData.java
@@ -0,0 +1,18 @@
+package login;
+
+import login.models.LoginData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestLoginData {
+    @Test
+    void LoginDataDoesNotModifyInputData() {
+        String username = "username";
+        String password = "password";
+        LoginData data = new LoginData(username, password);
+        assertEquals(username, data.username);
+        assertEquals(password, data.password);
+    }
+
+}
