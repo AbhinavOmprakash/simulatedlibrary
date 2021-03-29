f12b52a8bb4265b67cb9a6a92c03cba4234d5129
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 08:44:16 +0530

    cleanup and refactoring

--- src/main/java/signup/models/SignUpManager.java
+++ src/main/java/signup/models/SignUpManager.java
@@ -1,44 +1,73 @@
 package signup.models;
 
-import common.models.DataManager;
-import member.controllers.UserDataManager;
-import login.controllers.LoginDataManager;
-import common.models.Member;
-import admin.models.MembershipFactory;
-import login.models.LoginData;
+import common.Transaction;
+import common.customevents.CustomEvent;
+import common.customevents.EventCotroller;
+import common.models.*;
+import member.models.UserDataManager;
+import payment.models.DummyPayment;
+import common.models.PaymentGateway;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+//todo - refactor, class is doing too much.
 @SuppressWarnings({"rawtypes","unchecked"})
-public class SignUpManager{
+public class SignUpManager implements PaymentObserver{
     DataManager userDataManager = UserDataManager.getInstanceOf();
-    DataManager loginDataManager = new LoginDataManager();
-    DataManager accountsDataManager = new AccountsDataManager();
+    Map<String, SignUpData> signUpQueue = new HashMap<>();
+    PaymentGateway paymentGateway = DummyPayment.getInstanceOf();
+    CreateNewFinancialAcc accountCreator = new CreateNewFinancialAcc();
 
-    public boolean signUp(SignUpData data){
-        createMember(data);
-        createLoginData(data);
-        createFinancialAccount(data);
-        return true;
+    public SignUpManager() {
+        paymentGateway.registerObservers(this);
     }
 
-    private void createFinancialAccount(SignUpData data) {
-        List results = userDataManager.search(data.userName);
-        Member newMember = (Member) results.get(0);
-        FinancialAccount account = new FinancialAccount(newMember.getID());
-        accountsDataManager.addItem(account);
+    public void signUp(SignUpData data){
+        signUpQueue.put(data.userName, data);
+        // todo improve design. chained calls.
+        if(!(data.policy.name.equals("Basic"))) {
+            // can't call payment gateway before creating user.
+            forwardToPaymentGateway(data);
+            EventCotroller.getInstanceOf().dispatch(CustomEvent.PAYMENT_GATEWAY);
+        } else{
+            completeSignUp(data.userName);
+        }
     }
 
-    private void createLoginData(SignUpData data) {
-        LoginData loginData = new LoginData(data.userName, data.password);
-        loginDataManager.addItem(loginData);
+    @Override
+    public void receivePaymentStatus(String username, boolean status, double targetAmount, Transaction transaction) {
+        if(transaction.equals(Transaction.SIGNUP) && status){
+            completeSignUp(username);
+        }
+    }
+    private void forwardToPaymentGateway(SignUpData data) {
+        paymentGateway.initializePayment(data.userName, data.policy.membershipFees, Transaction.SIGNUP);
     }
 
 
-    private void createMember(SignUpData data) {
-        Member member = new Member(data.firstName, data.lastName, data.userName,
-                MembershipFactory.createMembership(data.policy));
-        userDataManager.addItem(member);
+    private void completeSignUp(String username) {
+        SignUpData data = signUpQueue.get(username);
+        signUpQueue.remove(username);
+
+        forwardToCreators(data);
+        changeCurrentUser(data.userName);
+
+        EventCotroller.getInstanceOf().dispatch(CustomEvent.SIGNED_UP);
+    }
+
+    //todo find better name
+    private void forwardToCreators(SignUpData data) {
+        CreateNewMember.createMember(data);
+        CreateNewLogin.createLoginCredentials(data);
+        accountCreator.enqueueCreation(data);
+    }
+
+    private void changeCurrentUser(String userName) {
+        List results = userDataManager.search(userName);
+        User newUser = (User) results.get(0);
+        CurrentUser.changeUser(newUser);
     }
 
 
