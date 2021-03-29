d72accd5085ae068eeb901fc61b7e767a37ad11b
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 08:43:37 +0530

    cleanup and refactoring

--- src/main/java/signup/models/SignUpData.java
+++ src/main/java/signup/models/SignUpData.java
@@ -1,19 +1,31 @@
 package signup.models;
 
 import common.models.MembershipPolicy;
+import member.models.MembershipPolicyManager;
 
+import java.util.List;
+
+@SuppressWarnings("rawtypes,unchecked")
 public class SignUpData {
-    public String firstName;
-    public String lastName;
-    public String userName;
-    public char[] password;
-    public MembershipPolicy policy;
+    private static final MembershipPolicyManager policyManager = new MembershipPolicyManager();
 
-    public SignUpData(String firstName, String lastName, String userName, char[] password, MembershipPolicy policy) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-        this.policy = policy;
+    protected String firstName;
+    protected String lastName;
+    protected String userName;
+    protected char[] password;
+    protected MembershipPolicy policy;
+
+    public SignUpData(RawSignUpData data) {
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.userName = data.userName;
+        this.password = data.password;
+        this.policy = getPolicy(data.policy);
     }
+    // todo- refactor class is doing too much
+    private MembershipPolicy getPolicy(String policyName) {
+        List results = policyManager.search(String.valueOf(policyName));
+        return (MembershipPolicy) results.get(0);
+    }
+
 }
