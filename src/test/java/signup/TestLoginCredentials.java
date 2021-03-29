75022551471a7b6d7555660637e9af858522dde3
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 10:11:27 +0530

    LoginData must store hashed password

---
+++ src/test/java/signup/TestLoginCredentials.java
@@ -0,0 +1,17 @@
+package signup;
+
+import login.models.LoginData;
+import login.models.RawLoginData;
+import org.junit.jupiter.api.Test;
+import signup.models.CredentialCreator;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class TestLoginCredentials {
+    @Test
+    void LoginCredentialsShouldStoreHashedPassword() {
+        RawLoginData data = new RawLoginData("ab","pass");
+        LoginData credentials =  CredentialCreator.createNewCredential(data);
+        assertNotEquals(data.getPasswd(), credentials.password);
+    }
+}
