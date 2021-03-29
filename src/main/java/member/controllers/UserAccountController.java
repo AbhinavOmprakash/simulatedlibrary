1721bc5fbd3cfcf456555527dcfd77adbf96cd76
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-03-29 08:48:31 +0530

    clean up and refactoring

--- src/main/java/member/controllers/UserAccountController.java
+++ src/main/java/member/controllers/UserAccountController.java
@@ -1,27 +1,20 @@
 package member.controllers;
 
-import common.controllers.GuiController;
-import common.controllers.MainFrameController;
-import common.views.MainJFrame;
 import member.views.MyAccount;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UserAccountController extends GuiController implements ActionListener {
-    MyAccount userAccount = new MyAccount(this);
-    public UserAccountController(MainFrameController parentController, MainJFrame mainFrame) {
-        super(parentController, mainFrame);
-        setCurrentPage(userAccount);
+public class UserAccountController implements ActionListener {
+
+    MyAccount page;
+
+    public UserAccountController(MyAccount page) {
+        this.page = page;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==userAccount.homeButton){
-            parentController.switchToHomePage();
-        } else if (e.getSource()==userAccount.upgradeMembershipButton){
-            parentController.switchToUpgradeMembership();
-        }
-
     }
+
 }
